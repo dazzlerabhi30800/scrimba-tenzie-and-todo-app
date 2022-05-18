58cd7829ebf4eef48e3a9d340ce78536d990e6c5
Abhishek <abhishek@pop-os.localdomain>
2022-05-18 11:04:08 +0530

    Toggle Dark Mode

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import Navbar from './Components/Navbar'
+import Main from './Components/Main'
 
 function App() {
+  
+  const [darkMode, setDarkMode] = useState(true);
+  
+
+  function toggleDarkMode() {
+    setDarkMode(prevMode => !prevMode)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar
+        darkMode={darkMode} 
+        toggleDarkMode={toggleDarkMode}
+      />
+      <Main
+        darkMode={darkMode}
+      />
     </div>
   );
 }
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Style.css";
+
+function Navbar(props) {
+  return (
+    <nav className={props.darkMode ? "dark": ""}>
+      <img src="../images/react-logo1.png" className="nav--logo--icon" alt="" />
+      <h3 className="nav--logo_text">ReactFacts</h3>
+      <div className="toggler">
+        <p className="toggler-light">Light</p>
+        <div className="toggler--slider" onClick={props.toggleDarkMode}>
+          <div className="toggler--slider--circle"></div>
+        </div>
+        <p className="toggler--dark">Dark</p>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
